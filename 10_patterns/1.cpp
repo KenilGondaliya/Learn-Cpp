ccdd65cd2f583fdbcd2dce536c2baa512afb3540
Kenil Gondaliya <kenilgondaliya960gmail.com>
2025-03-14 11:14:18 +0530

    commit

---
+++ 10_patterns/1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int i = 1;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << "*" << endl;
+            j++;
+        }
+
+        cout << "*" << endl;
+    }
+
+    return 0;
+}
+
+// *
