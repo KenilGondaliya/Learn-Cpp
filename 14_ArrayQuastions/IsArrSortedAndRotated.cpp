c983aabe720a2edf6c93513352afea9997e4a2eb
Kenil Gondaliya <kenilgondaliya960@gmail.com>
2025-03-20 18:10:36 +0530

    commit

--- 14_ArrayQuastions/IsArrSortedAndRotated.cpp
+++ 14_ArrayQuastions/IsArrSortedAndRotated.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+
+
+
 bool isArrSortedAndRotated(vector<int> nums)
 {
 
--- 15_String/ReverseWord.cpp
+++ 15_String/ReverseWord.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+void displayStr(vector<char> string)
+{
+
+    int n = string.size();
+
+    for (char i = 0; i < n; i++)
+    {
+        cout << string[i];
+    }
+}
+
+void reverseWord(vector<char> string)
+{
+
+    int n = string.size();
+
+    reverse(string.begin(), string.end());
+
+    displayStr(string);
+}
+
 int main()
 {
-    cout << "Hello, World!" << endl;
+    vector<char> string = {'k', 'e', 'n', 'i', 'l', ' ', 'g', 'o', 'n', 'd', 'a', 'l', 'i', 'y', 'a', ' ', 'i', 's', ' ', 'h', 'e', 'r', 'o'};
+
     return 0;
 }
