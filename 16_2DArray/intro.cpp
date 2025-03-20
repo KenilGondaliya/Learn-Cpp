8c6081f7dbbc59989e9dc0a4fe288cc0996fd7aa
Kenil Gondaliya <kenilgondaliya960@gmail.com>
2025-03-20 22:10:49 +0530

    commit

---
+++ 16_2DArray/intro.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // create a 2d array ➕
+    int arr[3][4];
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; i < 4; i++)
+    //     {
+    //         cin >> arr[i][j];
+    //     }
+        
+    // }
+
+    string name = " hello world";
+
+    cout << name;
+    
+    return 0;
+}
